b96dec1082acd6cb8d172e7dd92fb412bb6f706f
Jagath Jayachandran <75976924+jagathhhhh@users.noreply.github.com>
2024-08-01 18:23:25 +0530

    Create AudioSystem.java

---
+++ src/main/java/pianomain/AudioSystem.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jagath.pianomain;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author Jagath
+ */
+class AudioSystem {
+
+    static AudioInputStream getAudioInputStream(File file) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Clip getClip() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static AudioInputStream getAudioInputStream(String cUsersikkuvDesktopPianoC1wav) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
